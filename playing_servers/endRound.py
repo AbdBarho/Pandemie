ac69dc483ded63a499680fb57264d7fefa95bc65
Björn Rathjen <bjoern.rathjen@gmail.com>
2019-12-29 13:29:54 +0100

    added the endRound client for comparison

---
+++ playing_servers/endRound.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from bottle import post, request, run, BaseRequest
+
+@post("/")
+def index():
+	game = request.json
+	print(f'round: {game["round"]}, outcome: {game["outcome"]}')
+
+	action = {"type": "endRound"}
+	return action
+
+
+BaseRequest.MEMFILE_MAX = 10 * 1024 * 1024
+run(host="0.0.0.0", port=50123, quiet=True)
